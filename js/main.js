c8dd81ae2fe77a1328c769a1dd63ca105d316fc7
devops <devops@ebizzinfotech.com>
2024-04-17 18:46:41 +0530

    Slider added

--- js/main.js
+++ js/main.js
@@ -251,18 +251,3 @@
  
 })()
 
-
-
-
-$(document).ready(function () {
-  $("#news-slider").owlCarousel({
-      items: 3,
-      itemsDesktop: [1400, 3],
-      itemsDesktopSmall: [980, 2],
-      itemsMobile: [600, 1],
-      navigation: true,
-      navigationText: ["", ""],
-      pagination: true,
-      autoPlay: true,
-  });
-});
