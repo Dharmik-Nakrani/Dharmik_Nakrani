f5d8f1735bc424d44e948c7ee41cff85da05b9a6
DharmikN-Healtech <dharmik.n@healtech.au>
2025-06-19 13:22:12 +0530

    Refactor code structure for improved readability and maintainability

--- DharmikNakrani_Portfolio/settings.py
+++ DharmikNakrani_Portfolio/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,8 +93,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Media files
 MEDIA_URL = '/media/'
